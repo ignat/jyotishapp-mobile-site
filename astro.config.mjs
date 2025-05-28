e5bf75c6ed59a08e8f1bdcb742ab50adecf7317c
Ignat Skoryh <ignat.skoryh@gmail.com>
2025-05-28 16:49:55 +0300

    Prefetch pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   integrations: [],
+  prefetch: true,
   site: 'https://m.jyotishapp.com',
   vite: {
     plugins: [tailwindcss()],
